cf363dd9eb8e36b0a820c3d6a8250078b4efef96
SpunBlue <86245524+SpunBlue@users.noreply.github.com>
2022-08-04 23:42:00 -0400

    change to topdown rpg lol

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,8 +4,13 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var sillyPlayer:Player;
+
 	override public function create()
 	{
+		sillyPlayer = new Player(16, 16);
+		add(sillyPlayer);
+
 		super.create();
 	}
 
--- source/Player.hx
+++ source/Player.hx
@@ -6,31 +6,64 @@ import flixel.util.FlxColor;
 
 class Player extends FlxSprite
 {
-	public var defaultPlayerSpeed:Int = 130;
+	public static var allowGravity:Bool = true;
 
-	public var Decelleration:Int = 0;
-	public var Acceleration:Int = 0;
+	public var speed:Int = 130;
+	public var deceleration:Float;
 
 	public function new(x:Float, y:Float)
 	{
 		super(x, y);
 
-		makeGraphic(24, 24, FlxColor.BLUE);
+		// makeGraphic(48, 64, FlxColor.BLUE);
+		loadGraphic("assets/images/holoplayer.png", true, 48, 64);
+		antialiasing = false;
+		animation.add("idle", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 12, true);
+
+		animation.play("idle");
+
+		if (allowGravity)
+			acceleration.y = 500;
 	}
 
 	public function updateMovement()
 	{
-		var leftP:Bool = FlxG.keys.justPressed.LEFT;
-		var rightP:Bool = FlxG.keys.justPressed.RIGHT;
-		var upP:Bool = FlxG.keys.justPressed.UP;
+		var left = FlxG.keys.anyPressed([LEFT, A]);
+		var right = FlxG.keys.anyPressed([RIGHT, D]);
 
-		if (leftP && !rightP)
+		if (left)
+			velocity.x = speed * -1;
+		else if (right)
+			velocity.x = speed;
+
+		// Deceleration
+		if (velocity.x != 0 && !left && !right)
 		{
-			velocity.x = -defaultPlayerSpeed;
+			if (deceleration != speed / 5)
+				deceleration = speed / 5;
+
+			if (velocity.x > 0)
+				velocity.x -= deceleration;
+			else if (velocity.x < 0)
+				velocity.x += deceleration;
 		}
-		else if (rightP && !leftP)
+	}
+
+	function jumping()
+	{
+		var jump = FlxG.keys.anyPressed([UP, W, SPACE]);
+
+		if (this.isTouching(FLOOR) && jump)
 		{
-			velocity.x = defaultPlayerSpeed;
+			velocity.y = speed * -1.5; // probably not gonna keep it like this
 		}
+	}
+
+	override public function update(elapsed:Float)
+	{
+		jumping();
+
+		super.update(elapsed);
+		updateMovement();
 	}
 }
